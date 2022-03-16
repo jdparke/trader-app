0359a4c9d76a48d57b9ab243d7385b4e34ca86bb
John Parker <john.parker@us.softvision.global>
2022-03-16 18:18:23 -0400

    Cleaned up webhook

--- webhook.py
+++ webhook.py
@@ -12,29 +12,14 @@ def handle_github_hook():
 
   secret = str.encode(current_app.config.get('GITHUB_SECRET'))
 
-  #return jsonify({"secret":secret}), 200
-
   hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
   global repo
 
-  #return jsonify({"hashhex":hashhex},
-  #               {"signature": signature},
-  #              {"secret": secret},
-  #               {"compare": hmac.compare_digest(hashhex, signature)}
-  #              ), 200
-
-
   #if hmac.compare_digest(hashhex, signature):
   if 1==1:
-    #return jsonify({"got here":repo}), 200
 
     repo = Repo(current_app.config.get('REPO_PATH'))
-
     origin = repo.remotes.origin
-
-    #return jsonify({"got here":"got here"}), 200
-
-
     origin.pull('--rebase')
 
     commit = request.json['after'][0:6]
