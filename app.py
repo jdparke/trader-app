e0a5894fd0782ada53de225b8ff3d8fb107f75cf
John Parker <john.parker@us.softvision.global>
2022-03-16 18:11:57 -0400

    Good Updates.

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
+
 from flask import Flask
 from homepage import homepage
 from webhook import webhook
 
-app = Flask(__name__)
-app.register_blueprint(homepage)
-app.register_blueprint(webhook)
+def create_app():
+
+  app = Flask(__name__)
+  app.config['GITHUB_SECRET'] = os.environ.get('GITHUB_SECRET')
+  app.config['REPO_PATH'] = os.environ.get('REPO_PATH')
+  app.register_blueprint(homepage)
+  app.register_blueprint(webhook)
+
+  return(app)
