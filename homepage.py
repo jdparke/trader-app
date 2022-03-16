5708952b47f996f9f8610469dc3c36fca477b09f
John Parker <john.parker@us.softvision.global>
2022-03-16 17:49:19 -0400

    Test 19

--- homepage.py
+++ homepage.py
@@ -4,4 +4,4 @@ homepage = Blueprint('homepage', __name__)
 
 @homepage.route('/')
 def index():
-    return "<h1>Parker's Trader app NEW 18!!!</h1>"
+    return "<h1>Parker's Trader app NEW 19!!!</h1>"
