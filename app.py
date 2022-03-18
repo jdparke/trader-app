543fa174f09e4e2f6ec8bf33bfe395dd1a860270
John Parker <john.parker@us.softvision.global>
2022-03-18 16:51:43 -0400

    Got Ichimoku working!

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from page_home import page_home
 from page_download import page_download
 from calc_stat import calc_stat
+from calc_plot import calc_plot
 from webhook import webhook
 
 def create_app():
@@ -15,5 +16,6 @@ def create_app():
   app.register_blueprint(page_download)
   app.register_blueprint(webhook)
   app.register_blueprint(calc_stat)
+  app.register_blueprint(calc_plot)
 
   return(app)
